f0f7147a1fe48aa0b1251a337549cb418826d298
shafaprm <shafaprmdna2409@gmail.com>
2024-12-19 14:00:18 +0700

    intialiazing project

---
+++ src/test/java/com/tubes_pbo/_d_platformer/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tubes_pbo._d_platformer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
