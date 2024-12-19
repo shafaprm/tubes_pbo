1a96919071b7c5d2505af91fbf5ff7fce47f07e1
shafaprm <shafaprmdna2409@gmail.com>
2024-12-19 14:17:26 +0700

    Creating handlers folder which contains Content, Keys and LoggingHelper

---
+++ src/main/java/com/tubes_pbo/_d_platformer/handlers/LoggingHelper.java
@@ -0,0 +1,11 @@
+package com.tubes_pbo._d_platformer.handlers;
+
+import java.util.logging.Logger;
+
+public class LoggingHelper {
+    public static final Logger LOGGER = Logger.getLogger(LoggingHelper.class.getName());
+
+    private LoggingHelper() {
+        throw new IllegalStateException("Utility Class");
+    }
+}
