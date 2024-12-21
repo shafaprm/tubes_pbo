3c9676c225dfcb9d957fe22e25aabd17b3979ba7
erwineka123 <ekaerwin09@gmail.com>
2024-12-21 21:23:00 +0700

    update enemies

--- src/main/java/com/tubes_pbo/_d_platformer/model/enemies/Flyer.java
+++ src/main/java/com/tubes_pbo/_d_platformer/model/enemies/Flyer.java
@@ -3,38 +3,42 @@ package com.tubes_pbo._d_platformer.model.enemies;
 import com.tubes_pbo._d_platformer.model.Enemy;
 import com.tubes_pbo._d_platformer.tilemap.TileMap;
 
-public class Flyer extends Enemy{
-    private double[][] initValues = new double[][]{new double[]{4, 30, 30, 20, 26, 1, 1.5, 0.15, 4.0, -5},
-            new double[]{1, 25, 25, 20, 18, 1, 0.8, 0.15, 4.0, -5}};
+public class Flyer extends Enemy {
+    private final double[][] initValues = {
+            {4, 30, 30, 20, 26, 1, 1.5, 0.15, 4.0, -5},
+            {1, 25, 25, 20, 18, 1, 0.8, 0.15, 4.0, -5}
+    };
 
     public Flyer(TileMap tm, FlyerType type) {
         super(tm);
-        health = maxHealth = (int) initValues[type.value][0];
+        initializeAttributes(type.value);
+    }
 
-        width = (int) initValues[type.value][1];
-        height = (int) initValues[type.value][2];
-        cwidth = (int) initValues[type.value][3];
-        cheight = (int) initValues[type.value][4];
+    private void initializeAttributes(int typeIndex) {
+        health = maxHealth = (int) initValues[typeIndex][0];
 
-        damage = (int) initValues[type.value][5];
-        moveSpeed = initValues[type.value][6];
-        fallSpeed = initValues[type.value][7];
-        maxFallSpeed = initValues[type.value][8];
-        jumpStart = initValues[type.value][9];
+        width = (int) initValues[typeIndex][1];
+        height = (int) initValues[typeIndex][2];
+        cwidth = (int) initValues[typeIndex][3];
+        cheight = (int) initValues[typeIndex][4];
+
+        damage = (int) initValues[typeIndex][5];
+        moveSpeed = initValues[typeIndex][6];
+        fallSpeed = initValues[typeIndex][7];
+        maxFallSpeed = initValues[typeIndex][8];
+        jumpStart = initValues[typeIndex][9];
     }
 
     protected void getNextPosition() {
-        if (left)
-            dx = -moveSpeed;
-        else if (right)
-            dx = moveSpeed;
-        else
-            dx = 0;
+        dx = left ? -moveSpeed : right ? moveSpeed : 0;
+
         if (falling) {
             dy += fallSpeed;
-            if (dy > maxFallSpeed)
+            if (dy > maxFallSpeed) {
                 dy = maxFallSpeed;
+            }
         }
+
         if (jumping && !falling) {
             dy = jumpStart;
         }
@@ -45,8 +49,8 @@ public class Flyer extends Enemy{
 
         public final int value;
 
-        FlyerType(int val) {
-            value = val;
+        FlyerType(int value) {
+            this.value = value;
         }
     }
 }
