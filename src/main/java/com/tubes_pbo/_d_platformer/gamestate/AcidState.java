78e992136bf0ee3fbc080059e0c9a9fbd8c1f097
Muhammad Zul Rezky Zaaki <mhmmdzul@gmail.com>
2024-12-22 15:21:09 +0700

    Refactoring class AcidState, OptionState, HowToPlay

--- src/main/java/com/tubes_pbo/_d_platformer/gamestate/AcidState.java
+++ src/main/java/com/tubes_pbo/_d_platformer/gamestate/AcidState.java
@@ -2,13 +2,12 @@ package com.tubes_pbo._d_platformer.gamestate;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
-
-
 import com.tubes_pbo._d_platformer.audio.JukeBox;
 import com.tubes_pbo._d_platformer.handlers.Keys;
 import com.tubes_pbo._d_platformer.main.GamePanel;
 
-public class AcidState extends BasicState{
+public class AcidState extends BasicState {
+
     public AcidState(GameStateManager gsm) {
         super(gsm);
     }
@@ -21,47 +20,50 @@ public class AcidState extends BasicState{
     @Override
     public void draw(Graphics2D g) {
         super.draw(g);
+        drawBackground(g);
+        drawMessage(g);
+    }
+
+    private void drawBackground(Graphics2D g) {
         g.setColor(Color.YELLOW);
         g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
-        g.setFont(fontMenu);
-        g.setColor(Color.WHITE);
-        g.fillOval(185, 165, 250, 250); // draw a cycle OK
+
         g.setColor(Color.ORANGE);
-        g.fillOval(180, 160, 260, 260); // draw a cycle
-        // g.setColor(Color.ORANGE);
-        g.drawRect(185, 165, 250, 250); // draw a square Katerori
+        g.fillOval(180, 160, 260, 260);
         g.setColor(Color.WHITE);
-        g.fillOval(190, 170, 240, 240); // Fills a square
+        g.fillOval(185, 165, 250, 250);
         g.setColor(Color.YELLOW);
+        g.fillOval(190, 170, 240, 240);
+        g.setColor(Color.RED);
         g.fillOval(195, 175, 230, 230);
+    }
+
+    private void drawMessage(Graphics2D g) {
+        g.setFont(fontMenu);
         g.setColor(Color.RED);
         g.drawString("Congratulation!", 240, 280);
+
         g.setFont(font);
         g.drawString("Press any key to Play Again", 215, 305);
     }
 
     @Override
     protected void select() {
-        switch (currentChoice) {
-            case 0:
-                JukeBox.play("menuselect");
-                gsm.setState(GameStateManager.MENUSTATE);
-                break;
-            default:
-                JukeBox.play("menuselect");
-                gsm.setState(GameStateManager.MENUSTATE);
-                break;
-        }
+        JukeBox.play("menuselect");
+        gsm.setState(GameStateManager.MENUSTATE);
     }
 
     @Override
     public void handleInput() {
-        if (Keys.isPressed(Keys.ENTER))
+        if (Keys.isPressed(Keys.ENTER)) {
             select();
-        if (Keys.isPressed(Keys.UP) && currentChoice > 0) {
-            JukeBox.play("menuoption", 0);
-            gsm.setState(GameStateManager.MENUSTATE);
-            currentChoice--;
+        } else if (Keys.isPressed(Keys.UP) && currentChoice > 0) {
+            handleMenuNavigation();
         }
+    }
+
+    private void handleMenuNavigation() {
+        JukeBox.play("menuoption", 0);
+        currentChoice--;
     }
 }
