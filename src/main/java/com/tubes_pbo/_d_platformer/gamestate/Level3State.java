d2141e8f439b6c16b0bf6a89c9489cf0f2327c02
Ajax Falak S <ajax.afs26@gmail.com>
2024-12-24 16:54:07 +0700

    Refactor and fixing map

--- src/main/java/com/tubes_pbo/_d_platformer/gamestate/Level3State.java
+++ src/main/java/com/tubes_pbo/_d_platformer/gamestate/Level3State.java
@@ -4,7 +4,7 @@ import com.tubes_pbo._d_platformer.model.Enemy.EnemyType;
 import com.tubes_pbo._d_platformer.model.Player;
 import com.tubes_pbo._d_platformer.tilemap.Background;
 
-public class Level3State extends GameState{
+public class Level3State extends GameState {
     private boolean eventQuake;
 
     public Level3State(GameStateManager gsm) {
@@ -14,64 +14,90 @@ public class Level3State extends GameState{
 
     @Override
     public void init(int nextLevel) {
-
         super.init(nextLevel);
-        temple = new Background("/Backgrounds/temple1.gif", 0.5, 0);
+        setupLevelEnvironment();
+        setupEnemyConfiguration();
+        setupTitle(new int[]{0, 0, 193, 36});
+    }
 
+    private void setupLevelEnvironment() {
+        temple = createBackground("/Backgrounds/temple1.gif", 0.5);
         generateTileMap("/Maps/level3.map", 140, 0, false);
-
         setupGameObjects(300, 131, 2850, 371, false);
-        setupTitle(new int[] { 0, 0, 193, 36 });
         setupMusic("level2", "/Music/intro_theme.mp3", true);
+    }
 
-        enemyTypesInLevel = new EnemyType[] { EnemyType.XHELBAT, EnemyType.XHELBAT, EnemyType.XHELBAT,
-                EnemyType.XHELBAT, EnemyType.XHELBAT, EnemyType.XHELBAT, EnemyType.XHELBAT, EnemyType.XHELBAT,
-                EnemyType.ZOGU, EnemyType.ZOGU, EnemyType.ZOGU, EnemyType.ZOGU, EnemyType.UFO, EnemyType.UFO,
-                EnemyType.UFO, EnemyType.UFO, EnemyType.ZOGU, EnemyType.ZOGU, EnemyType.ZOGU, EnemyType.ZOGU };
-        coords = new int[][] { new int[] { 750, 100 }, new int[] { 900, 150 }, new int[] { 1320, 250 },
-                new int[] { 1570, 160 }, new int[] { 1590, 160 }, new int[] { 2600, 370 }, new int[] { 2620, 370 },
-                new int[] { 2640, 370 }, new int[] { 904, 130 }, new int[] { 1080, 270 }, new int[] { 1200, 270 },
-                new int[] { 1704, 300 }, new int[] { 1900, 580 }, new int[] { 2330, 550 }, new int[] { 2400, 490 },
-                new int[] { 2457, 430 }, new int[] {420, 400}, new int[] {340, 400}, new int[] {300, 425}, new int[] {45, 280} };
+    private Background createBackground(String path, double scrollSpeed) {
+        return new Background(path, scrollSpeed, 0);
+    }
 
+    private void setupEnemyConfiguration() {
+        enemyTypesInLevel = createEnemyTypes();
+        coords = createEnemyCoordinates();
         populateEnemies(enemyTypesInLevel, coords);
+    }
+
+    private EnemyType[] createEnemyTypes() {
+        return new EnemyType[]{
+            EnemyType.XHELBAT, EnemyType.XHELBAT, EnemyType.XHELBAT,
+            EnemyType.XHELBAT, EnemyType.XHELBAT, EnemyType.XHELBAT,
+            EnemyType.XHELBAT, EnemyType.XHELBAT, EnemyType.ZOGU,
+            EnemyType.ZOGU, EnemyType.ZOGU, EnemyType.ZOGU, EnemyType.UFO,
+            EnemyType.UFO, EnemyType.UFO, EnemyType.UFO, EnemyType.ZOGU,
+            EnemyType.ZOGU, EnemyType.ZOGU, EnemyType.ZOGU
+        };
+    }
+
+    private int[][] createEnemyCoordinates() {
+        return new int[][]{
+            {750, 100}, {900, 150}, {1320, 250}, {1570, 160},
+            {1590, 160}, {2600, 370}, {2620, 370}, {2640, 370},
+            {904, 130}, {1080, 270}, {1200, 270}, {1704, 300},
+            {1900, 580}, {2330, 550}, {2400, 490}, {2457, 430},
+            {420, 400}, {340, 400}, {300, 425}, {45, 280}
+        };
     }
 
     @Override
     public void update() {
-
         super.update();
-
-        // check if quake event should start
-        if (player.getx() > 2175 && !tileMap.isShaking()) {
+        if (shouldTriggerQuakeEvent()) {
             eventQuake = blockInput = true;
         }
-
-        if (eventQuake)
-            eventQuake();
-
+        if (eventQuake) {
+            handleQuakeEvent();
+        }
     }
 
-    // earthquake
-    private void eventQuake() {
+    private boolean shouldTriggerQuakeEvent() {
+        return player.getx() > 2175 && !tileMap.isShaking();
+    }
+
+    private void handleQuakeEvent() {
         eventCount++;
         if (eventCount == 1) {
-            player.stop();
-            player.setPosition(2175, player.gety());
-        }
-        if (eventCount == 60) {
+            triggerQuakeStart();
+        } else if (eventCount == 60) {
             player.setEmote(Player.CONFUSED_EMOTE);
-        }
-        if (eventCount == 120)
+        } else if (eventCount == 120) {
             player.setEmote(Player.NONE_EMOTE);
-        if (eventCount == 150)
+        } else if (eventCount == 150) {
             tileMap.setShaking(true, 10);
-        if (eventCount == 180)
+        } else if (eventCount == 180) {
             player.setEmote(Player.SURPRISED_EMOTE);
-        if (eventCount == 300) {
-            player.setEmote(Player.NONE_EMOTE);
-            eventQuake = blockInput = false;
-            eventCount = 0;
+        } else if (eventCount == 300) {
+            endQuakeEvent();
         }
+    }
+
+    private void triggerQuakeStart() {
+        player.stop();
+        player.setPosition(2175, player.gety());
+    }
+
+    private void endQuakeEvent() {
+        player.setEmote(Player.NONE_EMOTE);
+        eventQuake = blockInput = false;
+        eventCount = 0;
     }
 }
